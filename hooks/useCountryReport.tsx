6ffb021bd4594cdf74817131a0b2ff83aa902160
Mirian Quispe <mirianquispe1505@gmail.com>
2025-03-09 18:54:09 -0300

    feat: create useCountryReport hook for fetching and filtering country data

---
+++ hooks/useCountryReport.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { reportService } from '@/services/report-service'
+import { useQuery } from '@tanstack/react-query'
+import { useMemo, useState } from 'react'
+
+const REPORT_COUNTRIES_QUERY_KEY = 'report-countries'
+
+const useCountryReport = () => {
+  const [search, setSearch] = useState<string>('')
+
+  const { data, isLoading } = useQuery({
+    queryKey: [REPORT_COUNTRIES_QUERY_KEY],
+    queryFn: reportService.getAllCountries,
+    select: (data) => data.data
+  })
+
+  const countries = useMemo(
+    () =>
+      data?.filter((entry) =>
+        entry?.country?.toLowerCase().includes(search.toLowerCase())
+      ) || [],
+    [search]
+  )
+
+  return {
+    countries,
+    isLoading,
+    search,
+    setSearch
+  }
+}
+
+export default useCountryReport
+export type HookCountryReport = ReturnType<typeof useCountryReport>
