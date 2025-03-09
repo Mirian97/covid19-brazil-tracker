1144d07cd846509eab1b13469a5a372a35e1586c
Mirian Quispe <mirianquispe1505@gmail.com>
2025-03-09 17:26:21 -0300

    feat: add custom hook for fetching state reports by date

---
+++ hooks/useStateByDateReport.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { reportService } from '@/services/report-service'
+import { useQuery } from '@tanstack/react-query'
+import { useState } from 'react'
+
+const REPORT_STATES_BY_DATE_QUERY_KEY = 'report-states-by-date'
+
+const useStateReportByDate = () => {
+  const [selectedDate, setSelectedDate] = useState('')
+
+  const { data, isLoading } = useQuery({
+    queryKey: [REPORT_STATES_BY_DATE_QUERY_KEY, selectedDate],
+    queryFn: () => reportService.getStatesByDate(selectedDate.replaceAll('-', '')),
+    enabled: !!selectedDate,
+    select: (data) => data.data
+  })
+
+  return {
+    data,
+    isLoading,
+    selectedDate,
+    setSelectedDate
+  }
+}
+
+export default useStateReportByDate
