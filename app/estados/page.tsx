596389650bf447ade57ee8e3f6e34d126d94f160
Mirian Quispe <mirianquispe1505@gmail.com>
2025-03-09 15:17:34 -0300

    feat: add state detail and list components for COVID-19 data visualization

--- app/estados/page.tsx
+++ app/estados/page.tsx
@@ -1,3 +1,36 @@
+'use client'
+import { CustomSelect } from '@/components/select'
+import { Title } from '@/components/title'
+import { states } from '@/constants/states'
+import useStateReport from '@/hooks/useStateReport'
+import { StateDetail } from './_components/state-detail'
+import { StateList } from './_components/state-list'
+
 export default function PerStatesPage() {
-  return <div></div>
+  const { statesData, stateData, selectedState, setSelectedState, isLoadingState } =
+    useStateReport()
+
+  return (
+    <>
+      <Title
+        title='Dados por Estado'
+        description='Visualize os dados da COVID-19 por estado brasileiro.'
+      />
+      <CustomSelect
+        isClearable
+        options={states}
+        value={selectedState}
+        className='max-w-md'
+        placeholder='Selecione um estado'
+        onChange={(value) => setSelectedState(value)}
+      />
+      <div className='flex w-full flex-1 flex-col'>
+        {selectedState?.value ? (
+          <StateDetail stateData={stateData} isLoadingState={isLoadingState} />
+        ) : (
+          <StateList statesData={statesData} />
+        )}
+      </div>
+    </>
+  )
 }
---
+++ providers/all-providers.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { queryClient } from '@/config/query-client'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { FC, PropsWithChildren } from 'react'
+
+export const AllProviders: FC<PropsWithChildren> = ({ children }) => {
+  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+}
