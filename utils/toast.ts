313cc0f1dab86a0886193ffdd08892545fadd4c5
Mirian Quispe <mirianquispe1505@gmail.com>
2025-03-09 21:49:53 -0300

    style: update toast component styles for improved readability and consistency

--- utils/toast.ts
+++ utils/toast.ts
@@ -7,9 +7,9 @@ export const toast = (options: SweetAlertOptions) => {
     iconColor: 'white',
     icon: 'success',
     customClass: {
-      popup: 'colored-toast text-white rounded-lg',
+      popup: 'colored-toast !text-white rounded-lg',
       title: '!font-bold !text-lg',
-      htmlContainer: '!font-semibold !text-sm'
+      htmlContainer: '!font-semibold !text-base'
     },
     showConfirmButton: false,
     timer: 4000,
