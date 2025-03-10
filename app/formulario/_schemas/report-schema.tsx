c51f657072df610c8043f89964a55964980b7b57
Mirian Quispe <mirianquispe1505@gmail.com>
2025-03-09 21:49:16 -0300

    feat: add report form schema for COVID-19 data validation

---
+++ app/formulario/_schemas/report-schema.tsx
@@ -0,0 +1,39 @@
+import * as z from 'zod'
+
+export const reportFormSchema = z.object({
+  state: z.string({
+    required_error: 'Selecione um estado.'
+  }),
+  cases: z.coerce
+    .number({
+      required_error: 'Informe o número de casos.',
+      invalid_type_error: 'Informe um número válido.'
+    })
+    .nonnegative('O número deve ser positivo.'),
+  confirmed: z.coerce
+    .number({
+      required_error: 'Informe o número de casos confirmados.',
+      invalid_type_error: 'Informe um número válido.'
+    })
+    .nonnegative('O número deve ser positivo.'),
+  deaths: z.coerce
+    .number({
+      required_error: 'Informe o número de óbitos.',
+      invalid_type_error: 'Informe um número válido.'
+    })
+    .nonnegative('O número deve ser positivo.'),
+  recovered: z.coerce
+    .number({
+      required_error: 'Informe o número de recuperados.',
+      invalid_type_error: 'Informe um número válido.'
+    })
+    .nonnegative('O número deve ser positivo.'),
+  date: z
+    .string()
+    .date('Formato de data inválida.')
+    .refine((date) => new Date(date) < new Date(), {
+      message: 'Data de vencimento deve ser posterior a hoje'
+    })
+})
+
+export type ReportFormSchema = z.infer<typeof reportFormSchema>
